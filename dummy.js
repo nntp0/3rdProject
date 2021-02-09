cc2b6bfec3e680de46bf81ba959a550d29e29ef0
dh-sudo <76761951+dh-sudo@users.noreply.github.com>
2021-02-09 09:34:39 +0900

    초기설정

---
+++ dummy.js
@@ -0,0 +1,41 @@
+const dummyDatabase =
+[
+  {
+    "id": 1,
+    "name": "새우깡",
+    "price": "1000",
+    "manufacturer": "농심"
+  },
+  {
+    "id": 2,
+    "name": "포카칩",
+    "price": "1500",
+    "manufacturer": "농심"
+  },
+  {
+    "id": 3,
+    "name": "홈런볼",
+    "price": "2000",
+    "manufacturer": "해태"
+  },
+  {
+    "id": 4,
+    "name": "꼬북칩",
+    "price": "2500",
+    "manufacturer": "오리온"
+  },
+  {
+    "id": 5,
+    "name": "쫄병스낵",
+    "price": "3000",
+    "manufacturer": "농심"
+  },
+  {
+    "id": 6,
+    "name": "알새우칩",
+    "price": "3500",
+    "manufacturer": "농심"
+  }
+]
+
+module.exports = dummyDatabase;
